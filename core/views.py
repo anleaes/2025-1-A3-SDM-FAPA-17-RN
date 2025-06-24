a06e86a151ffb9d5732849ff3c6f34e5108cacce
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 20:26:08 -0300

    Cria view TurmaListCreateView para listagem e criação

--- core/views.py
+++ core/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
-from .models import Aluno
-from .serializers import AlunoSerializer
-from .models import Professor
-from .serializers import ProfessorSerializer
+from .models import Aluno, Professor, Turma
+from .serializers import AlunoSerializer, ProfessorSerializer, TurmaSerializer
 
 
 class AlunoListCreateView(generics.ListCreateAPIView):
@@ -21,3 +19,7 @@ class ProfessorListCreateView(generics.ListCreateAPIView):
 class ProfessorRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Professor.objects.all().order_by('nome')
     serializer_class = ProfessorSerializer
+
+class TurmaListCreateView(generics.ListCreateAPIView):
+    queryset = Turma.objects.all()
+    serializer_class = TurmaSerializer
