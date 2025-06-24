2be14d13b653d4af5c3ba41347c2ed9c7078d74a
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 18:54:57 -0300

    Ajuste de ordenação no queryset de Aluno

--- core/views.py
+++ core/views.py
@@ -3,9 +3,10 @@ from .models import Aluno
 from .serializers import AlunoSerializer
 
 class AlunoListCreateView(generics.ListCreateAPIView):
-    queryset = Aluno.objects.all()
+    queryset = Aluno.objects.all().order_by('nome')
     serializer_class = AlunoSerializer
 
+
 class AlunoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Aluno.objects.all()
+    queryset = Aluno.objects.all().order_by('nome')
     serializer_class = AlunoSerializer
