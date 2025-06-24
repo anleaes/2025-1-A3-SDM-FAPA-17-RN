9251a799636f3022af042e2d57ca2ba3354a1139
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 18:43:02 -0300

    Criação do model Aluno

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome Completo")
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(unique=True)
+    matricula = models.CharField(max_length=20, unique=True)
+    curso = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
