e0f0fa55f492ebc0e5e88f64d2812bb86ace72ab
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 19:09:57 -0300

    Adiciona rota de listagem e criação para Professor

--- core/urls.py
+++ core/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path, include
 from django.contrib import admin
-from .views import AlunoListCreateView
-from .views import AlunoRetrieveUpdateDestroyView
+from .views import AlunoListCreateView, AlunoRetrieveUpdateDestroyView, ProfessorListCreateView, ProfessorListCreateView
 
 urlpatterns = [
     path('alunos/', AlunoListCreateView.as_view(), name='aluno-list-create'),
     path('alunos/<int:pk>/', AlunoRetrieveUpdateDestroyView.as_view(), name='aluno-detail'),
-]
-
-urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('professores/', ProfessorListCreateView.as_view(), name='professor-list-create'),
 ]
 
