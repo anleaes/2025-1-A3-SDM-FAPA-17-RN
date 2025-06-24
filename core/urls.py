67afcfd46768750d43713281bcb62b5e8015b4e8
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 18:58:59 -0300

    Adiciona rota de detalhe para Aluno

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import AlunoListCreateView
+from .views import AlunoRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('alunos/', AlunoListCreateView.as_view(), name='aluno-list-create'),
+    path('alunos/<int:pk>/', AlunoRetrieveUpdateDestroyView.as_view(), name='aluno-detail'),
 ]
+
