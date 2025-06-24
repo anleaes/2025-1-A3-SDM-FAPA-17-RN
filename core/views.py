88f3e0f477d1141bdf745c1b08b52818f8dc7bff
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 18:54:12 -0300

    Criação das views de listagem e detalhe do Aluno

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Aluno
+from .serializers import AlunoSerializer
 
-# Create your views here.
+class AlunoListCreateView(generics.ListCreateAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class AlunoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
