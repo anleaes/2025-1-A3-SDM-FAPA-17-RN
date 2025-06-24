11c306a8f4b839c5b638b76463caccf0217aa04d
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 19:03:32 -0300

    Criação inicial do serializer para Professor

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Aluno
+from .models import Professor
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = '__all__'
         read_only_fields = ('id',)  
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = '__all__'
