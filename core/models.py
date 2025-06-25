933a112393c543b64a941ae506df21a6aae03e17
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-25 18:03:00 -0300

    feat: cria model Perfil

--- core/models.py
+++ core/models.py
@@ -74,3 +74,12 @@ class CarrinhoMatricula(models.Model):
 
     def __str__(self):
         return f"Carrinho de {self.aluno.nome} - Status: {self.status}"
+
+class Perfil(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
