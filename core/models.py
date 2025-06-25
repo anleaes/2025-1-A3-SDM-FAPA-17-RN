4e4f341d49f5c5b5c89f7450dfd5349a55c859c7
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-24 21:22:02 -0300

    ajustes

--- core/models.py
+++ core/models.py
@@ -20,15 +20,6 @@ class Professor(models.Model):
     def __str__(self):
         return self.nome
 
-class Disciplina(models.Model):
-    nome = models.CharField(max_length=100)
-    carga_horaria = models.PositiveIntegerField()
-    turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='disciplinas')
-    professor = models.ForeignKey('Professor', on_delete=models.CASCADE, related_name='disciplinas')
-
-    def __str__(self):
-        return self.nome
-
 class Turma(models.Model):
     nome = models.CharField(max_length=50)
     ano = models.IntegerField()
@@ -41,5 +32,14 @@ class Turma(models.Model):
 
     def __str__(self):
         return f"{self.nome} - {self.ano} - {self.periodo}"
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100)
+    carga_horaria = models.PositiveIntegerField()
+    turma = models.ForeignKey('Turma', on_delete=models.CASCADE, related_name='disciplinas')
+    professor = models.ForeignKey('Professor', on_delete=models.CASCADE, related_name='disciplinas')
+
+    def __str__(self):
+        return self.nome
 
 
