be120e2fdbc8ecb1fddac522efc9da0b30bdeb09
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-25 17:39:03 -0300

    feat: cria serializer da entidade Frequencia

--- core/serializers.py
+++ core/serializers.py
@@ -38,4 +38,11 @@ class DisciplinaSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
 
+class FrequenciaSerializer(serializers.ModelSerializer):
+    aluno = serializers.StringRelatedField()
+
+    class Meta:
+        model = Frequencia
+        fields = '__all__'
+        read_only_fields = ('id',)
 
