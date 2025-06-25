e9593ce9fac546b30cb9416e4d845a2495076aeb
MatheusDanosk1 <matheusdanoskipetuco@gmail.com>
2025-06-25 18:17:47 -0300

    chore: importa views da entidade Usuario para urls

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,8 @@ from .views import (
     CarrinhoMatriculaRetrieveUpdateDestroyView,
     PerfilListCreateView,
     PerfilRetrieveUpdateDestroyView,
+    UsuarioListCreateView,
+    UsuarioRetrieveUpdateDestroyView,
 )
 
 
